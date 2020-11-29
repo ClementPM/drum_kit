449185b1a230d57e33a0351a6a0c1362968d69b0
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-11-30 00:24:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const numberOfDrumbButton = document.querySelectorAll('.drum').length;
+
+for (i = 0; i<numberOfDrumbButton; i++) {
+document.querySelectorAll(".drum")[i].addEventListener("click", (event) => {
+    alert('i got clicked');
+  });
+}
+
+//   switch (buttonClick) {
+//     case "q":
+//       new Audio('./sounds/tom-1.mp3').play();
+//       break;
+
+//     case "s":
+//         const tom2 = new Audio('./sounds/tom-2.mp3').play();
+//       break;
+
+//       case "d":
+//         const tome3 = new Audio('./sounds/tom-3.mp3')
+//         tome3.play();
+//       break;
+
+//       case "f":
+//         const tome4 = new Audio('./sounds/tom-4.mp3')
+//         tome4.play();
+//       break;
+
+//       case "j":
+//         const snare = new Audio('./sounds/snare.mp3')
+//         snare.play();
+//       break;
+
+//       case "k":
+//         const crash = new Audio('./sounds/crash.mp3')
+//         crash.play();
+//       break;
+
+//       case "l":
+//         const kick = new Audio('./sounds/kick-bass.mp3')
+//         kick.play();
+//       break;
+
+//     default:
+//     break;
+//   };
+// });
+
+
+
+
